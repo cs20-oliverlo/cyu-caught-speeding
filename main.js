5adafc09fde52dcdc0aa4b55cdb63efa31c31bc2
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-03 10:49:49 -0600

    awd

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // Caught Speeding CYU Asgn
+
+document.getElementById("btn").addEventListener("click", determineTicket);
+let ticketOutput = document.getElementById("ticket-out");
+
+function determineTicket() {
+let driverSpeed = document.getElementById("d-speed").value;
+let limitSpeed = document.getElementById("l-speed").value;
+let speedDifference = (driverSpeed - limitSpeed)
+if (speedDifference >= 20) {
+    ticketOutput.innerHTML = "Big Ticket";
+} else if (speedDifference <= 20 && speedDifference >= 1) {
+    ticketOutput.innerHTML = "Small Ticket";
+} else {
+    ticketOutput.innerHTML = "No Ticket";
+}
+}
